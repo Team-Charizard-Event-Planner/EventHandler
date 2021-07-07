a8ca31308b91536ea2ff306189dfe4dd9b2b3b6b
Jeff Chen <contact.jeffchen@gmail.com>
2021-07-07 10:06:51 -0700

    minor change in loginpage

--- client/components/loginPage.js
+++ client/components/loginPage.js
@@ -5,38 +5,37 @@ import { Button, TextField } from "@material-ui/core";
 const LoginPage = () => {
   // const dispatch = useDispatch();
 
-  const handleLogin = (e) => {
+  const handleChange = (e) => {
     e.preventDefault();
     console.log("hello");
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    fetch('/user/login', {
-      method: 'POST',
+    fetch("/user/login", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         //need redux store
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data === "incorrect username/password")
+          console.log("incorrect password");
+        else {
+          fetch("/user/verify", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({}),
+          });
+        }
       })
-    })
-    .then((res) => res.json())
-    .then((data) => {
-      if(data === 'incorrect username/password') console.log('incorrect password')
-      else {
-        fetch('/user/verify', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-
-          })
-        })
-      }
-    })
-    .catch((err) => console.log(err));
+      .catch((err) => console.log(err));
   };
 
   return (
@@ -45,18 +44,16 @@ const LoginPage = () => {
         id="LoginForm"
         method="POST"
         action="/api/user/login"
-        onSubmit={handleLogin}
+        onSubmit={handleSubmit}
       >
-      {/* need to add in action here for redux */}
-        <TextField label="email" name="email" onChange={handleSubmit} />
+        {/* need to add in action here for redux */}
+        <TextField label="email" name="email" onChange={handleChange} />
         <br></br>
-      {/* need to add in action here for redux */}
-        <TextField label="password" name="password" onChange={handleSubmit} />
+        {/* need to add in action here for redux */}
+        <TextField label="password" name="password" onChange={handleChange} />
         <br></br>
       </form>
-      <Button
-        type="submit" variant="contained" color="secondary"
-      >
+      <Button type="submit" variant="contained" color="secondary">
         Login
       </Button>
     </div>

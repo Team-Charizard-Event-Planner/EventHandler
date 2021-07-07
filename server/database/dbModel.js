df0c058e82abbddf1ca52d72630124078f3ab8a8
Thomas Reeder <thomaseugenereeder@gmail.com>
2021-07-07 08:38:53 -0700

    minor tweak

--- server/database/dbModel.js
+++ server/database/dbModel.js
@@ -1,9 +1,6 @@
 const { Pool } = require('pg');
 
-const PG_URI = 'postgres://vkdwvgkr:ONAuGYK17XUBWyruTzfgHvIbFhFDVj7d@kashin.db.elephantsql.com/vkdwvgkr';
-
-// const PG_URI = process.env.PG_URI;
-
+const PG_URI = process.env.PG_URI;
 
 const pool = new Pool ({
   connectionString: PG_URI

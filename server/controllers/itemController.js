0b6a7056f55081b94f3f85a17e3c19b0dce49e0c
Thomas Reeder <thomaseugenereeder@gmail.com>
2021-07-08 00:50:48 -0700

    more controllers and routers

--- server/controllers/itemController.js
+++ server/controllers/itemController.js
@@ -1,11 +1,54 @@
-const db = ('../schemas/schema');
+const db = require('../schemas/schema');
 
 const itemController = {};
 
-// GET ALL ITEMS
+// GET ALL ITEMS FOR EVENT
+itemController.getAllByEvent = (req, res, next) => {
+  const { event_id } = req.params;
+  const params = [event_id];
+
+  const query = `SELECT i.*, u.username
+  FROM items i
+  LEFT OUTER JOIN users u ON i.user_id = u._id
+  WHERE event_id = $1;`;
+
+  db.query(query, params)
+    .then(data => {
+      console.log('data in getAllItems', data);
+      res.locals.items = data.rows;
+      return next();
+    })
+    .catch(err => {
+      console.log('err in getAllItems', err);
+      return next(err);
+    });
+};
 
 // CREATE ITEM
-// return item from create item
+itemController.create = (req, res, next) => {
+  const { item_name, event_id } = req.body;
+  const params = [item_name, event_id];
+
+  const query = `INSERT INTO items
+  (item_name, event_id)
+  VALUES ($1, $2)
+  RETURNING _id, item_name, event_id;`;
+
+  console.log('params', params);
+  console.log('sending query', query);
+
+  db.query(query, params)
+    .then(data => {
+      console.log('data in createItem', data);
+
+      res.locals.item = data.rows[0];
+      return next();
+    })
+    .catch(err => {
+      console.log('err in createItem', err);
+      return next(err);
+    });
+};
 
 // CLAIM ITEM
 
--- server/routers/itemRouter.js
+++ server/routers/itemRouter.js
@@ -4,14 +4,40 @@ const itemController = require('../controllers/itemController');
 const router = express.Router();
 
 // GET ALL ITEMS BY EVENT
+router.get('/:event_id',
+  itemController.getAllByEvent,
+  (req, res) => {
+    return res.status(200).json(res.locals.items);
+  }
+);
 
 // CREATE ITEM
+router.post('/',
+  itemController.create,
+  (req, res) => {
+    return res.status(200).json(res.locals.item);
+  }
+);
 
 // CLAIM ITEM ("edit")
+router.put('/claim',
+  (req, res) => {
+    return res.status(200).json('success');
+  }
+);
 
 // REMOVE ITEM
+router.delete('/',
+  (req, res) => {
+    return res.status(200).json('success');
+  }
+);
 
 // EDIT ITEM? (ie name, cost?)
-
+router.put('/edit',
+  (req, res) => {
+    return res.status(200).json('success');
+  }
+);
 
 module.exports = router;

21b6642c9733d7f80e725e615e2af0aa853cfde0
Thomas Reeder <thomaseugenereeder@gmail.com>
2021-07-07 23:29:31 -0700

    server routes & controllers

--- server/routers/eventRouter.js
+++ server/routers/eventRouter.js
@@ -1,35 +1,40 @@
 const express = require('express');
 const eventController = require('../controllers/eventController');
+const attendeeController = require('../controllers/attendeeController');
+
 const router = express.Router();
 
-
 // get all events a user has been invited to
-router.get('/',
+router.get('/:userID',
   eventController.getEvents,
   (req, res) => {
-    return res.status(200).json('get all events')
+    return res.status(200).json(res.locals.events)
   }
 );
 
 // get specific
-router.get('/:id',
 // eventController GET ONE
-(req, res) => {
-  return res.status(200).json('get one event');
-  });
+router.get('/:id',
+  (req, res) => {
+    return res.status(200).json('get one event');
+  }
+);
 
 // create event
 router.post('/create',
   eventController.create,
+  attendeeController.addUserAfterCreate,
   (req, res) => {
-    return res.status(200).json(res.locals.event);
-});
+    return res.status(200).json({ event: res.locals.event, host: res.locals.host });
+  }
+);
 
 // edit events
+// eventController UPDATE
 router.put('/',
-  // eventController UPDATE
   (req, res) => {
     return res.status(200).json('edit event');
-});
+  }
+);
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -5,9 +5,8 @@ const cookieParser = require('cookie-parser');
 
 const authRouter = require('./routers/authRouter');
 const eventRouter = require('./routers/eventRouter');
-// const userRouter = require("./routers/userRouter");
-
-// app.use("/api/user", userRouter);
+const attendeeRouter = require('./routers/attendeeRouter');
+const itemRouter = require('./routers/itemRouter');
 
 const app = express();
 const PORT = 3000;
@@ -21,14 +20,12 @@ app.use(cookieParser());
 // routes
 // static routes
 
-// /auth for user authentication pathways
+// main routes
 app.use('/auth', authRouter);
-
-// /event for event fetching & creation
 app.use('/event', eventRouter);
-
+app.use('/attendee', attendeeRouter);
+app.use('/item', itemRouter);
 // should event include item? should item be separate?
-
 
 // serve HTML
 app.get('/', (req, res) => {
